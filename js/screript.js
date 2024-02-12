adfce16ced332451aa97ac31947b11ff93edab01
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-02-12 21:35:00 +0600

    first commit

---
+++ js/screript.js
@@ -0,0 +1,25 @@
+function triangleCalculator () {
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base  = parseFloat(triangleBaseText);
+    // console.log(base);
+
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText);
+
+
+    const area = 0.5 * base * height;
+    // console.log(area);
+
+    const triangleAreaSpan = document.getElementById('triangle-area');
+    triangleAreaSpan.innerText = area;
+
+
+}
+
+
+
+function rectangleCalculator() {
+    console.log('rectangleCalculator');
+}
